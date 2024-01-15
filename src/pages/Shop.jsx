c6076b6b56264c4113df5fc644afa414986561ba
mdnazmul42726 <nazmulofficial@outlook.com>
2024-01-15 23:44:32 +0600

    added filter section on the shop page

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,4 +1,26 @@
+import { Plus } from 'phosphor-react'
+import { Accordion } from 'keep-react'
+import { useEffect, useState } from 'react';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+
 const Shop = () => {
+
+    const [cars, setCars] = useState([]);
+
+    const { data = [] } = useQuery({
+        queryKey: ['shop', 'cars'],
+        queryFn: async () => {
+            const response = await axios.get('http://localhost:5000/cars/v1');
+            return setCars(response.data)
+        }
+    });
+
+    async function handleFilterData(filed, data) {
+        const response = await axios.get(`http://localhost:5000/cars/v1?filed=${filed}&data=${data}`);
+
+    }
+
     return (
         <div className="min-h-screen bg-[#141A1C]">
             <div className=" bg-[url('https://i.ibb.co/6JmKc1S/shop-banner.jpg')] h-96 bg-fixed bg-cover">
@@ -8,6 +30,141 @@ const Shop = () => {
                         <input type="text" name="" id="" placeholder="Car Name..." className="py-2 px-4 rounded-l active:!border-0" />
                         <input type="submit" value="Search" className="py-2 px-2 rounded-r text-white cursor-pointer hover:bg-[#947d5d] bg-[#BFA37C]" />
                     </form>
+                </div>
+            </div>
+            <div className="flex gap-4">
+
+                {/* Filter bar */}
+                <div className="w-1/5 bg-[#241C2B]  p-5">
+                    <div className="flex justify-between items-center border-b pb-2 mb-10">
+                        <h3 className='text-3xl text-white font-semibold uppercase'>Filter</h3>
+                        <h3 className=' text-slate-300 '>Clear Filter</h3>
+
+                    </div>
+                    <Accordion className='space-y-6'>
+
+                        {/* Body Type */}
+                        <Accordion.Panel className="">
+                            <Accordion.Container className="bg-metal-900 text-white group-hover:bg-metal-800">
+                                <Accordion.Title className="text-white">Body Type</Accordion.Title>
+                                <Accordion.Icon>
+                                    <Plus size={24} color="#fff" />
+                                </Accordion.Icon>
+                            </Accordion.Container>
+                            <Accordion.Content className="text-white">
+                                <div className="space-y-3">
+                                    <option className="hover:underline cursor-pointer" onClick={() => handleFilterData('bodyType', 'SUV Coupe Sedan')}>SUV Coupe Sedan</option>
+                                    <option className="hover:underline cursor-pointer" onClick={() => handleFilterData('bodyType', 'Crossover')}>Crossover</option>
+                                    <option className="hover:underline cursor-pointer" onClick={() => handleFilterData('bodyType', 'Hatchback')}>Hatchback</option>
+                                    <option className="hover:underline cursor-pointer" onClick={() => handleFilterData('bodyType', 'Soft Top Convertible')}>Soft Top Convertible</option>
+                                    <option className="hover:underline cursor-pointer" onClick={() => handleFilterData('bodyType', 'Sports Car Van')}>Sports Car Van</option>
+                                    <option className="hover:underline cursor-pointer" onClick={() => handleFilterData('bodyType', 'Wagon')}>Wagon</option>
+                                    <option className="hover:underline cursor-pointer" onClick={() => handleFilterData('bodyType', 'Utility Truck')}>Utility Truck</option>
+                                    <option className="hover:underline cursor-pointer" onClick={() => handleFilterData('bodyType', 'Other')}>Other</option>
+                                </div>
+                            </Accordion.Content>
+                        </Accordion.Panel>
+
+                        {/* Fuel Type */}
+                        <Accordion.Panel className="">
+                            <Accordion.Container className="bg-metal-900 text-white group-hover:bg-metal-800">
+                                <Accordion.Title className="text-white">
+                                    Fuel Type</Accordion.Title>
+                                <Accordion.Icon>
+                                    <Plus size={24} color="#fff" />
+                                </Accordion.Icon>
+                            </Accordion.Container>
+                            <Accordion.Content className="text-white">
+                                <div className="space-y-3">
+                                    <option className="hover:underline cursor-pointer">Diesel</option>
+                                    <option className="hover:underline cursor-pointer">Petrol</option>
+                                    <option className="hover:underline cursor-pointer">Hybrid</option>
+                                    <option className="hover:underline cursor-pointer">Electric</option>
+                                </div>
+                            </Accordion.Content>
+                        </Accordion.Panel>
+
+                        {/* transmissionType */}
+                        <Accordion.Panel className="">
+                            <Accordion.Container className="bg-metal-900 text-white group-hover:bg-metal-800">
+                                <Accordion.Title className="text-white">
+                                    Transmission Type</Accordion.Title>
+                                <Accordion.Icon>
+                                    <Plus size={24} color="#fff" />
+                                </Accordion.Icon>
+                            </Accordion.Container>
+                            <Accordion.Content className="text-white">
+                                <div className="space-y-3">
+                                    <option className="hover:underline cursor-pointer">Manual Transmission</option>
+                                    <option className="hover:underline cursor-pointer">Automatic Transmission</option>
+                                </div>
+                            </Accordion.Content>
+                        </Accordion.Panel>
+
+                        {/* regional Spec */}
+                        <Accordion.Panel className="">
+                            <Accordion.Container className="bg-metal-900 text-white group-hover:bg-metal-800">
+                                <Accordion.Title className="text-white">
+                                    Regional Spec</Accordion.Title>
+                                <Accordion.Icon>
+                                    <Plus size={24} color="#fff" />
+                                </Accordion.Icon>
+                            </Accordion.Container>
+                            <Accordion.Content className="text-white">
+                                <div className="space-y-3">
+                                    <option className="hover:underline cursor-pointer">GCC Specs</option>
+                                    <option className="hover:underline cursor-pointer">American Specs</option>
+                                    <option className="hover:underline cursor-pointer">Canadian Specs</option>
+                                    <option className="hover:underline cursor-pointer">European Specs</option>
+                                    <option className="hover:underline cursor-pointer">Japanese Specs</option>
+                                    <option className="hover:underline cursor-pointer">Korean Specs</option>
+                                    <option className="hover:underline cursor-pointer">Chinese Specs</option>
+                                    <option className="hover:underline cursor-pointer">Other</option>
+                                </div>
+                            </Accordion.Content>
+                        </Accordion.Panel>
+
+                        {/* steeringSide */}
+                        <Accordion.Panel className="">
+                            <Accordion.Container className="bg-metal-900 text-white group-hover:bg-metal-800">
+                                <Accordion.Title className="text-white">
+                                    Steering Side</Accordion.Title>
+                                <Accordion.Icon>
+                                    <Plus size={24} color="#fff" />
+                                </Accordion.Icon>
+                            </Accordion.Container>
+                            <Accordion.Content className="text-white">
+                                <div className="space-y-3">
+                                    <option className="hover:underline cursor-pointer">Left Hand</option>
+                                    <option className="hover:underline cursor-pointer">Right Hand</option>
+                                </div>
+                            </Accordion.Content>
+                        </Accordion.Panel>
+
+                        {/* condition */}
+                        <Accordion.Panel className="">
+                            <Accordion.Container className="bg-metal-900 text-white group-hover:bg-metal-800">
+                                <Accordion.Title className="text-white">
+                                    Condition</Accordion.Title>
+                                <Accordion.Icon>
+                                    <Plus size={24} color="#fff" />
+                                </Accordion.Icon>
+                            </Accordion.Container>
+                            <Accordion.Content className="text-white">
+                                <div className="space-y-3">
+                                    <option className="hover:underline cursor-pointer">Excellent</option>
+                                    <option className="hover:underline cursor-pointer">Good</option>
+                                    <option className="hover:underline cursor-pointer">Fair</option>
+                                </div>
+                            </Accordion.Content>
+                        </Accordion.Panel>
+                    </Accordion>
+                </div>
+
+                {/* content */}
+                <div className="w-4/5 border min-h-screen p-5 flex gap-4">
+
+                    {cars.map(car => <div key={car._id} className="border w-72 h-72"></div>)}
                 </div>
             </div>
         </div>
