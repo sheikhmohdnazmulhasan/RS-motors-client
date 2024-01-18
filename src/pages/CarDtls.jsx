43395b1a811734dd0ba6987c847e4050d0711eec
rakibaakter <rakibaakter635@gmail.com>
2024-01-18 21:04:57 +0600

    load data to detail page

--- src/pages/CarDtls.jsx
+++ src/pages/CarDtls.jsx
@@ -3,9 +3,23 @@ import { useLoaderData } from "react-router-dom";
 const CarDtls = () => {
   const data = useLoaderData();
   console.log(data);
+  const {
+    bodyType,
+    condition,
+    features,
+    fuelType,
+    mileage,
+    photo,
+    price,
+    regionalSpec,
+    steeringSide,
+    title,
+    transmissionType,
+    year,
+  } = data;
 
   return (
-    <div>
+    <div className="min-h-screen bg-[#000] text-white">
       <p>Details Information</p>
     </div>
   );
